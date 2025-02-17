fa0dea24cf0b81c1affd450ae6550faa3babe9d4
Phoenix5050 <59894902+Phoenix5050@users.noreply.github.com>
2025-02-17 14:24:42 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// Story elements arrays
+const characters = ['a pirate', 'a robot', 'a princess', 'a dragon'];
+const actions = ['found a treasure', 'solved a riddle', 'rescued a friend', 'flew to the moon'];
+const locations = ['in a forest', 'on a mountain', 'under the sea', 'in a castle'];
+
+// Initialize selected story parts
+let selectedCharacter = '';
+let selectedAction = '';
+let selectedLocation = '';
